a4ab36d6f34e024fd72990077976de13c3a58f6c
khushijangir2007 <ashajangir082@gmail.com>
2026-09-05 18:21:21 +0530

    Add basic to-do list functionality

---
+++ Todo_list.py
@@ -0,0 +1,12 @@
+#razk1- to do list 
+tasks = []
+print("To-Do List")
+while True:
+    task = input("Enter a task (or type done to stop): ")
+    if task.lower() == "done":
+    break
+    tasks.append(task)
+
+print("\nMy Tasks:")
+for i, task in enumerate(tasks, 1):
+    print(i, task)
